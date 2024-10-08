fd710fc7e0d50ceadf4e6930c88a116aeb11a045
filoutigrou <filoudede33@gmail.com>
2024-10-08 16:21:54 +0200

    Client.py

--- Client.py
+++ Client.py
@@ -1,7 +1,1 @@
-import discord
 
-token = "MTI5MjA5NTI5NDU0ODQ3NTk0NA.GxNavU.Bj4G7lYFCBxtnWyG82L5aSJi1GHgAAU_AGtmGA"
-
-client = discord.Client(intents=discord.Intents.all())
-
-client.run(token=token)
