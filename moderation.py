81883070c8d8cb591c06742f2d936529e74d506f
filoutigrou <filoudede33@gmail.com>
2024-10-08 16:20:10 +0200

    moderation.py

--- moderation.py
+++ moderation.py
@@ -1,15 +1,1 @@
-import discord
-from discord.ext import commands
 
-class ModerationCog(commands.Cog):
-    def __init__(self, bot):
-        self.bot = bot
-    
-    @commands.command()
-    @commands.bot_has_permissions(kick_members=True)
-    async def kick(self, ctx, member: discord.Member, *,reason=None):
-        await member.kick(reason=reason)
-        await ctx.send(f'{member.name} a été exclu(e).')
-
-async def setup(bot):
-    await bot.add_cog(ModerationCog(bot))
